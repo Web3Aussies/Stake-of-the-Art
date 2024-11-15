93ba55e0548c1b6f8134692fd8f796b2061609e7
Sarmaad Amin <sarmaad@gmail.com>
2024-11-15 22:00:39 +0700

    added worldapp basic react setup with flowbite and tailwindcss

---
+++ frontend/worldapp/src/providers/QueryProvider.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: 10 * 60 * 1000,
+        },
+    },
+});
+
+export default function QueryProvider({ children }: { children: ReactNode }) {
+    return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+}

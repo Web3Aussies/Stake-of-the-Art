5d67c35dc540e13e2a2116d75afc8453f940c509
Broden Suffern <broden.suffern24@gmail.com>
2024-11-16 12:13:28 +1100

    Tested responding to recieved messages and added logging for all messages.

--- node/src/bot/index.ts
+++ node/src/bot/index.ts
@@ -2,6 +2,8 @@ import { Client } from "@xmtp/xmtp-js";
 import dotenv from "dotenv";
 import { JsonRpcProvider, Wallet } from "ethers";
 import HandlerContext from "./context";
+import { createLogger } from "./logger";
+import Command from "./command";
 
 dotenv.config();
 
@@ -13,8 +15,30 @@ export default async function () {
     const signer = new Wallet(`0x${process.env.XMTP_KEY!}`, provider);
     const client = await Client.create(signer, { env: "production" });
 
+    const logger =  createLogger(true, "info", "bot", {
+        walletAddress: client.address
+    });
+
+    // Create array of commands that the bot can handle
+    const bots: Command[] = [];
+
+
     // Stream all messages to test if setup works
     for await (const message of await client.conversations.streamAllMessages()) {
-        console.log("Message recieved");
+        logger.info({messageId: message.id }, "Recieved message");
+        
+        console.log(`Message id: ${message.id}`);
+
+        let processed = false;
+
+        // need to check if sender address is our own address and ignore
+        if (message.senderAddress === client.address) {
+            continue;
+        }
+
+        // Do simple reply for now
+        const conversation = message.conversation;
+
+        await conversation.send("Hello world");
     }
 }

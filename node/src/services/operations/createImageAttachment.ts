2171ad4f87f800bea3fc2af9742415fd0b047bb1
Broden Suffern <broden.suffern24@gmail.com>
2024-11-17 01:32:35 +1100

    Added the create share part of the createImageAttachment service for XMTP

--- node/src/services/operations/createImageAttachment.ts
+++ node/src/services/operations/createImageAttachment.ts
@@ -2,6 +2,14 @@ import { WithId } from "mongodb";
 import { User } from "../../models/user";
 import { v4 as uuidv4 } from "uuid";
 import { AttachmentCodec, RemoteAttachmentCodec } from "@xmtp/content-type-remote-attachment";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export type CreateShareResponse = {
+    shareId: string,
+    uploadUrl: string
+}
 
 export async function CreateImageAttachment(imageUrl: string, context: WithId<User>) {
     // Download file from hot storage
@@ -32,4 +40,22 @@ export async function CreateImageAttachment(imageUrl: string, context: WithId<Us
     );
 
     console.log(encyrptedEncoded);
+
+    // Create share to get upload url
+
+    const createRes = await fetch(`${process.env.DOTNET_ENDPOINT_URL}/app/shares`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${context.token}`
+        },
+        body: JSON.stringify({
+            filename: attachment.filename,
+            contentType: "application/octet-stream"
+        }),
+    });
+
+    const createShare: CreateShareResponse = await createRes.json();
+
+    console.log("Create share response: ", createShare);
 }

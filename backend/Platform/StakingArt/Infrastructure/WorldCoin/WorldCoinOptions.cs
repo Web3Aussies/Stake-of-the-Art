fa0976229895620300daed19393381848acc2a6f
Sarmaad Amin <sarmaad@gmail.com>
2024-11-16 08:28:26 +0700

    added woldcoin backend service with refit

---
+++ backend/Platform/StakingArt/Infrastructure/WorldCoin/WorldCoinOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace StakingArt.Infrastructure.WorldCoin;
+
+public class WorldCoinOptions
+{
+    public static string Section = "WorldCoin";
+    public string AppId { get; set; }
+    public string BaseUrl { get; set; }
+    public string AttestationAction { get; set; }
+}

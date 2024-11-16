5667667fcaaff278df1228061c04b2eeaf9ba247
Broden Suffern <broden.suffern24@gmail.com>
2024-11-17 00:01:47 +1100

    Updated info XMTP command to display a nicer welcome message for non-registered and registered users. Edited sample XMTP command to work on non-registered users.

--- node/src/bot/operations/info.ts
+++ node/src/bot/operations/info.ts
@@ -5,12 +5,19 @@ async function handle({ message, context }: HandlerContext) {
     const conversation = message.conversation;
 
     if (context) {
-        await conversation.send("Welcome back");
+        await conversation.send(`Welcome back to Stake of the Art.
+- type 'balance' to check your balance
+- type 'deposit' to deposit funds
+- type 'list # [categories]' to see the top # wallpapers in different categories
+- type 'categories' to see wallpaper categories
+- reply 'download' to a wallpaper to download`);
         return;
     }
 
     await conversation.send(
-        `Welcome to Stake of the Art`
+        `Welcome to Stake of the Art.
+        - type 'register' to open an account
+        - type 'sample' to see a sample of the art collection`
     );
 
     return;
--- node/src/bot/operations/sample.ts
+++ node/src/bot/operations/sample.ts
@@ -7,11 +7,6 @@ dotenv.config();
 async function handle({ message, context }: HandlerContext) {
     const conversation = message.conversation;
 
-    // Check context
-    if (!context) {
-        await conversation.send("Please register with us before seeing our sample wallpaper.");
-    }
-
     // Fetch the sample image
     await conversation.send(`This a sample of our art:\n${process.env.SAMPLE_URL}`);
 }
